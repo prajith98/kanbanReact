66c5b5b87673f9950e6a53209d8eceafe6860beb
Prajith Nair <prajithnair98@gmail.com>
2020-05-05 02:49:51 +0530

    Updates

---
+++ precache-manifest.ba7deb3eb8d8c144c7b4cc0259c66712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877cdf807d7109bb88e80e5822797e02",
+    "url": "/kanbanReact/index.html"
+  },
+  {
+    "revision": "9e57ce398e2d2b22541f",
+    "url": "/kanbanReact/static/css/main.a6d8e54c.chunk.css"
+  },
+  {
+    "revision": "eb77f573ddb6c9c80940",
+    "url": "/kanbanReact/static/js/2.bab11813.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kanbanReact/static/js/2.bab11813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e57ce398e2d2b22541f",
+    "url": "/kanbanReact/static/js/main.06439d15.chunk.js"
+  },
+  {
+    "revision": "8d7b0a682fb08c6c57e1",
+    "url": "/kanbanReact/static/js/runtime-main.423bceae.js"
+  }
+]);
